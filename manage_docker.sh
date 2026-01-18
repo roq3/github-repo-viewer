0ddbcf17725bd5993f1642748c813835ac1a6830
roque <gitlab@roque.pl>
2026-01-18 18:30:40 +0100

    New styles

--- manage_docker.sh
+++ manage_docker.sh
@@ -21,8 +21,13 @@ case $COMMAND in
     echo "Wyświetlanie logów kontenera..."
     docker-compose logs -f
     ;;
+  dev)
+    echo "Uruchamianie wersji deweloperskiej z nodemon..."
+    docker-compose down
+    docker run --rm -it -v $(pwd):/app -w /app -p 7123:3000 node:current-alpine sh -c "npm install && npx nodemon server.js"
+    ;;
   *)
-    echo "Użycie: ./manage_docker.sh {up|down|restart|logs}"
+    echo "Użycie: ./manage_docker.sh {up|down|restart|logs|dev}"
     exit 1
     ;;
 esac
